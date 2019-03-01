fc38fd58081bc4f2fc83fb6b14778771ff215dcb
Maor Kachlon <maooor13@gmail.com>
2019-03-01 15:41:38 +0200

    Add check PIN def.

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -10,14 +10,26 @@ def print_balance():
 def commands():
     pass
 
-def check_pin(PIN):
+
+
+def check_pin(accounts, account, PIN):
+    counter = 3
+    print("You entered {}.".format(PIN))
+    while accounts[0][account] != PIN and counter:
+            print("You've enter wrong PIN.\n"
+                  "You have {} more tries.\n".format(counter))
+            counter = counter - 1
+            PIN = input("Enter PIN number.\n")
+    if accounts[0][account] == PIN:
+        return True
+    return False
     pass
 
 # This function gets all accounts and waits
 # for user to input his account number.
 def get_bank_acc_number(accounts):
     account_number = input("Please enter your bank account number.\n")
-    while account_number not in accounts[1]:
+    while account_number not in accounts[0]:
         account_number = input("Try again.\n"
                                "Please enter your bank account number.")
     answer = input("The account number that you entered is {}.\n"
@@ -55,6 +67,10 @@ def run_bank():
     print(accounts)
     account_number = get_bank_acc_number(accounts)
     print("Account number:{}".format(account_number))
+    while check_pin(accounts, account_number, input("Enter PIN number.\n")) == False:
+        print("You have reached the maximum amount of attempts.\n"
+              "Try again later.")
+        return
 
 
 
