a6d9532ec74a1c0e7889e9ea55b4935796d0ac24
Maor Kachlon <maooor13@gmail.com>
2019-03-01 14:39:37 +0200

    Plan all def of exercise

---
+++ exercises/exercise1.py
@@ -0,0 +1,30 @@
+def change_pin():
+    pass
+
+def withdraw():
+    pass
+
+def print_balance():
+    pass
+
+def commands():
+    pass
+
+def check_pin():
+    pass
+
+def get_bank_acc_number():
+    pass
+
+def init_account():
+    pass
+
+def run_bank():
+    pass
+
+def main():
+    run_bank()
+
+
+if __name__ == '__main__':
+    main()
