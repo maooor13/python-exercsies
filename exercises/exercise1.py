94cbecea9890a90628bd3e82276b6084d8407b1f
Maor Kachlon <maooor13@gmail.com>
2019-03-01 14:54:17 +0200

    Add init account def.

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -16,11 +16,26 @@ def check_pin():
 def get_bank_acc_number():
     pass
 
-def init_account():
-    pass
+def init_accounts():
+    # Account is a variable that contains two dictionaries
+    # First dictionary is acc number and PIN
+    # Second dictionary is acc number and balance
+    accounts = {
+        "123456":"1234",
+        "598123":"5486",
+        "112358":"8510",
+        "000000":"0000"
+    },{
+        "123456": 5000,
+        "598123": 84320,
+        "112358": -120,
+        "000000": 0
+    }
+    return accounts
 
 def run_bank():
-    pass
+    accounts = init_accounts()
+    print(accounts)
 
 def main():
     run_bank()
