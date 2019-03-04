f06d5903cf47d44080eda45ab88f5f7c911ee38b
Maor Kachlon <maooor13@gmail.com>
2019-03-04 22:42:28 +0200

    Add is_int func so list can have negative.

--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -2,6 +2,12 @@ def main():
     get_list()
     pass
 
+def is_int(num):
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
 
 def get_list():
     sum = 0
@@ -9,12 +15,14 @@ def get_list():
     print("When you want to stop type \"Stop\".")
     num = input("Enter Number.\n")
     while num.lower() != "stop":
-        if num.isdigit():
+        if is_int(num):
             sum = sum + int(num)
         else:
             print("Invalid input, ignoring.")
         num = input("Enter Number.\n")
     print("The sum is {}.".format(sum))
+
+
 
 
 if __name__ == '__main__':
