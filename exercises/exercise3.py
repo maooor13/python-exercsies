6e9cf995d747046adb9a748e75d60cd05ece3912
Maor Kachlon <maooor13@gmail.com>
2019-03-04 23:09:10 +0200

    Add skeleton of exercise.

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -1,5 +1,32 @@
 def main():
+    game = [[1, 2, 0],
+            [2, 1, 0],
+            [2, 1, 1]]
+    print("The current state of the game is {}".format(check_state(game)))
     pass
+
+
+def check_rows(game):
+    pass
+
+
+def check_cols(game):
+    pass
+
+
+def check_diag(game):
+    pass
+
+
+def check_state(game):
+    if check_rows(game):
+        pass
+    if check_cols(game):
+        pass
+    if check_diag(game):
+        pass
+
+
 
 
 if __name__ == '__main__':
