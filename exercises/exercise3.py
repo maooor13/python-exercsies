7d02a305ff53c3b569fcc9466f39ce429e7f1ffb
Maor Kachlon <maooor13@gmail.com>
2019-03-05 00:26:32 +0200

    Add check diagonals function.

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -1,8 +1,8 @@
 def main():
     game = [[1, 0, 1],
-            [2, 0, 2],
-            [0, 0, 0]]
-    print("The current state of the game is {}".format(check_state(game)))
+            [2, 1, 1],
+            [2, 1, 1]]
+    winner = check_state(game)
     pass
 
 
@@ -36,17 +36,48 @@ def check_cols(game):
     pass
 
 
+def check_main_diag(game):
+    if game[0][0] == game[1][1] == game [2][2]:
+        return game[1][1]
+    return 0
+    pass
+
+
+def check_second_diag(game):
+    winner = game[1][1]
+    prev = winner
+    for i in range(len(game[0])):
+        for j in range(len(game[0])):
+            if i + j == len(game[0])-1:
+                if game[i][j] != winner:
+                    return 0
+    return winner
+
+    pass
+
+
 def check_diag(game):
+    winner = 0
+    if check_main_diag(game) != 0:
+        winner = check_main_diag(game)
+    elif check_second_diag(game) != 0:
+        winner = check_second_diag(game)
+    return winner
     pass
 
 
 def check_state(game):
     if check_rows(game):
+        print("The winner is {}!\nHe won with a row.".format(check_rows(game)))
         return check_rows(game)
     elif check_cols(game):
-        pass
+        print("The winner is {}!\nHe won with a column.".format(check_cols(game)))
+        return check_cols(game)
     elif check_diag(game):
-        pass
+        print("The winner is {}!\nHe won with a diagonal.".format(check_diag(game)))
+        return check_diag(game)
+    pass
+    print("The current game is tied.")
     return 0
 
 
