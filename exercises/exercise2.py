70f008b684c1af84f330a9308808783d76a0b12f
Maor Kachlon <maooor13@gmail.com>
2019-03-04 22:28:31 +0200

    Add get_list function

---
+++ exercises/exercise2.py
@@ -0,0 +1,21 @@
+def main():
+    get_list()
+    pass
+
+
+def get_list():
+    sum = 0
+    print("Enter numbers until you want to stop.")
+    print("When you want to stop type \"Stop\".")
+    num = input("Enter Number.\n")
+    while num.lower() is not "stop":
+        if num.isdigit():
+            sum = sum + int(num)
+        else:
+            print("Invalid input, ignoring.")
+        num = input("Enter Number.\n")
+        print("The sum is {}.".format(sum))
+
+
+if __name__ == '__main__':
+    main()
