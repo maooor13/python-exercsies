a7e99021a994c7da230edebcb55e4459c32f8be4
Maor Kachlon <maooor13@gmail.com>
2019-03-04 23:31:25 +0200

    Add check rows and check row functions.

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -1,14 +1,23 @@
 def main():
-    game = [[1, 2, 0],
-            [2, 1, 0],
-            [2, 1, 1]]
+    game = [[1, 0, 1],
+            [2, 0, 2],
+            [0, 0, 0]]
     print("The current state of the game is {}".format(check_state(game)))
     pass
 
 
 def check_rows(game):
+    winner = 0
+    for i in range(3):
+        winner = check_row(game, i)
+        if winner != 0:
+            return winner
     pass
 
+def check_row(game, i):
+    if len(game[i]) == game[i].count(game[i][0]):
+        return game[i][0]
+    return 0
 
 def check_cols(game):
     pass
@@ -20,12 +29,12 @@ def check_diag(game):
 
 def check_state(game):
     if check_rows(game):
+        return check_rows(game)
+    elif check_cols(game):
         pass
-    if check_cols(game):
+    elif check_diag(game):
         pass
-    if check_diag(game):
-        pass
-
+    return 0
 
 
 
