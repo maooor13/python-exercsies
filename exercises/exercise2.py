b1eeea72a45d25e8e7a3e2fde9a3349309fd4653
Maor Kachlon <maooor13@gmail.com>
2019-03-04 22:34:32 +0200

    Fix bug

--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -8,13 +8,13 @@ def get_list():
     print("Enter numbers until you want to stop.")
     print("When you want to stop type \"Stop\".")
     num = input("Enter Number.\n")
-    while num.lower() is not "stop":
+    while num.lower() != "stop":
         if num.isdigit():
             sum = sum + int(num)
         else:
             print("Invalid input, ignoring.")
         num = input("Enter Number.\n")
-        print("The sum is {}.".format(sum))
+    print("The sum is {}.".format(sum))
 
 
 if __name__ == '__main__':
