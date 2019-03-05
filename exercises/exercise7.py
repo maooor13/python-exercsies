a41e030d9d1b542f6a658e2f02bc7dbab689b83a
Maor Kachlon <maooor13@gmail.com>
2019-03-05 21:27:37 +0200

    Add decorator function.

--- exercises/exercise7.py
+++ exercises/exercise7.py
@@ -1,14 +1,34 @@
 factorial_decor = {}
 
 def main():
-    print(factorial(500))
+    # Can't do factorial for 998 or over.
+    try:
+        factorial(998)
+    except:
+        print("I can't do factorial of 998, too much recursion :(")
+    # Decoring the max
+    factorial(997)
+    # Can do 1992 factorial.
+    print(factorial(1992))
     pass
+
+
+def decorator(x):
+    if x in factorial_decor:
+        return factorial_decor[x]
+
+
 
 
 def factorial(x):
     if x == 1:
         return 1
-    return x * factorial(x-1)
+    if decorator(x):
+        return decorator(x)
+    else:
+        answer = x * factorial(x-1)
+        factorial_decor[x] = answer
+        return answer
 
 
 if __name__ == '__main__':
