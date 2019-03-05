f8e8074df846a08d076af8c80c5db9d0268b8b63
Maor Kachlon <maooor13@gmail.com>
2019-03-05 20:45:48 +0200

    Add map function.

--- exercises/exercise6.py
+++ exercises/exercise6.py
@@ -1,5 +1,20 @@
 def main():
+    list = [1,2,3]
+    print(list)
+    list = Map(add5,list)
+    print(list)
     pass
+
+
+def add5(x):
+    return x+5
+
+
+def Map(f,list):
+    new_list = []
+    for x in list:
+        new_list.append(f(x))
+    return new_list
 
 
 if __name__ == '__main__':
